1bab6cddeb1e2067ee4528cf76343a25474e5eb8
konstanin <kostia505@gmail.com>
2024-12-10 09:30:31 +0100

    db connection: added

---
+++ db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const mongoUri = process.env.MONGO_URI || "URI";
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri, {});
+    console.log("connected to mongo");
+  } catch (err) {
+    console.error("faild to connect to mongo", err.message);
+  }
+};
+
+export default connectDB;
